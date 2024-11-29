c45a5b39efd555c87323fe81b7371f445d24919b
britneypearl <Bsimamu3212@conestogac.on.ca>
2024-11-28 22:09:44 -0500

    Added functions to calculate angles

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -30,3 +30,16 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 
     return result;
 }
+
+void calculateAngles(int side1, int side2, int side3) {
+    if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
+        double angleA = acos((pow(side2, 2) + pow(side3, 2) - pow(side1, 2)) / (2.0 * side2 * side3)) * (180.0 / M_PI);
+        double angleB = acos((pow(side1, 2) + pow(side3, 2) - pow(side2, 2)) / (2.0 * side1 * side3)) * (180.0 / M_PI);
+        double angleC = 180.0 - angleA - angleB;
+
+        printf("The angles of the triangle are: A = %.2f°, B = %.2f°, C = %.2f°\n", angleA, angleB, angleC);
+    }
+    else {
+        printf("The given sides do not form a triangle, so angles cannot be calculated.\n");
+    }
+}
