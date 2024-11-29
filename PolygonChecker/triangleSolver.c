3e517e91d8a34e9358e0a175eceed3f1057afd86
britneypearl <Bsimamu3212@conestogac.on.ca>
2024-11-28 22:05:35 -0500

    I defined the constant PI, which wil be used for angle calculations.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,29 +1,9 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <math.h> // For trigonometric functions
+#include <math.h>
 
 #include "triangleSolver.h"
 
-#define PI 3.14159265358979323846
-
-
-char* analyzeTriangle(int side1, int side2, int side3) {
-	char* result = "";
-	if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
-		result = "Not a triangle";
-	}
-	else if (side1 == side2 && side1 == side3) {
-		result = "Equilateral triangle";
-	}
-	else if ((side1 == side2 && side1 != side3) || 
-		(side1 == side3 && side1 != side2))
-	{
-		result = "Isosceles triangle";
-	}
-	else {
-		result = "Scalene triangle";
-	}
-
-	return result;
-}
-
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
