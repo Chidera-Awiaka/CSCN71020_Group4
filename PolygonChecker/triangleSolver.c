98e0dee1285fbd1c1a598340a0b8e09cec10343b
britneypearl <Bsimamu3212@conestogac.on.ca>
2024-11-28 22:08:34 -0500

    Added and modified the conditional statement.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -7,3 +7,26 @@
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+
+char* analyzeTriangle(int side1, int side2, int side3) {
+    char* result = "";
+    if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
+        result = "Not a triangle";
+    }
+    else if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+        result = "Not a triangle";
+    }
+    else if (side1 == side2 && side1 == side3) {
+        result = "Equilateral triangle";
+    }
+    else if ((side1 == side2 && side1 != side3) ||
+        (side1 == side3 && side1 != side2) ||
+        (side2 == side3 && side2 != side1)) {
+        result = "Isosceles triangle";
+        }
+    else {
+        result = "Scalene triangle";
+    }
+
+    return result;
+}
