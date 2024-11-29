f44c71f3fba87c865b2b8bdf6eddaf51f8c73e6d
Imchidex <144730149+Imchidex@users.noreply.github.com>
2024-11-28 21:15:30 -0500

    Added rectangle detection test

--- RectangleTest/RectangleTest.cpp
+++ RectangleTest/RectangleTest.cpp
@@ -5,12 +5,12 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace RectangleTest
 {
-	TEST_CLASS(RectangleTest)
-	{
-	public:
-		
-		TEST_METHOD(TestMethod1)
-		{
-		}
-	};
-}
+    TEST_CLASS(RectangleTest)
+    {
+    public:
+
+        TEST_METHOD(TestRectangleDetection)
+        {
+            int points[4][2] = { {0, 0}, {0, 2}, {3, 2}, {3, 0} };
+            Assert::IsTrue(isRectangle(points));
+        }
